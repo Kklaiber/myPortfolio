d7a3e989e068271f8d1716a5f982dd8773d41390
Kyle Klaiber <Kylesklaiber@gmail.com>
2019-02-21 23:08:18 -0800

    cleaned up code and fixed couple bugs with contact modal, still needs styling

--- main.js
+++ main.js
@@ -6,59 +6,35 @@ window.addEventListener('scroll', function(){
 });
 
 
-// var phoneModal = document.getElementById('myPhoneModal');
-// var emailModal = document.getElementById('myEmailModal');
-// var emailBtn = document.getElementById('email');
-// var phoneBtn = document.getElementById('phone');
+var phoneModal = document.getElementById('myPhoneModal');
+var emailModal = document.getElementById('myEmailModal');
+var emailBtn = document.getElementById('email');
+var phoneBtn = document.getElementById('phone');
 
-// var span = document.getElementsByClassName('close')[0];
-
-// phoneBtn.onclick = function(){
-//     phoneModal.style.display = 'block';
-// }
-
-// emailBtn.onclick = function(){
-//     emailModal.style.display = 'block';
-// }
-
-// span.onclick = function(){
-//     phoneModal.style.display = 'none';
-//     emailModal.style.display = 'none';
-// }
-
-// window.onclick = function(event){
-//     if (event.target == phoneModal) {
-//         phoneModal.style.display = 'none'
-//     } else if (event.target == emailModal) {
-//         emailModal.style.display = 'none';
-//     }
-// }
+var span = document.getElementsByClassName('close')[0];
 
 
-
-
-// Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
+phoneBtn.onclick = function(){
+    phoneModal.style.display = "inherit";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+emailBtn.onclick = function(){
+    emailModal.style.display = "inherit";
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+span.onclick = function(){
+    phoneModal.style.display = 'none';
+    emailModal.style.display = 'none';
 }
+
+window.onclick = function(event){
+    if (event.target == phoneModal) {
+        phoneModal.style.display = 'none'
+    } else if (event.target == emailModal) {
+        emailModal.style.display = 'none';
+    }
+}
+
+
+
+
